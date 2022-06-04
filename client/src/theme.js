fa0fbbb30ea91810bcc37eaba5091f537e7c72fa
Dan <danii-benitez@hotmail.com>
2022-06-04 15:48:02 -0300

    [mod] color theme

--- client/src/theme.js
+++ client/src/theme.js
@@ -2,8 +2,8 @@ import { extendTheme, theme } from "@chakra-ui/react"
 
 export default extendTheme({
   colors: {
-    primary: theme.colors.blackAlpha[700],
-    secondary: theme.colors.blackAlpha[200],
+    primary: theme.colors.gray[700],
+    secondary: theme.colors.gray[200],
     textPrimary: theme.colors.white,
     textSecondary: theme.colors.gray[400],
   },
